75356340f42990f15e0a39ea424ba04bded64025
Alexander <som17son25@gmail.com>
2023-04-06 21:49:23 +0300

    fix: redux-store create AppDispatch(type) and AppThunk(type)

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -1,12 +1,10 @@
-import {Action, applyMiddleware, combineReducers, createStore} from "redux";
+import {Action, AnyAction, applyMiddleware, combineReducers, legacy_createStore as createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {navbarReducer} from "./sidebar-reducer";
-import {
-    usersReducer
-} from "./users-reducer";
+import {usersReducer} from "./users-reducer";
 import {authReducer} from "./auth-reducer";
-import thunkMiddleware, {ThunkAction} from "redux-thunk";
+import thunkMiddleware, {ThunkAction, ThunkDispatch} from "redux-thunk";
 import {reducer as formReducer} from 'redux-form'
 import appReducer from "./app-reducer";
 
@@ -19,15 +17,21 @@ export const rootReducer = combineReducers({
     form: formReducer,
     app: appReducer,
 });
-
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 export type InferActionsType<T> = T extends { [keys: string]: (...args: any[]) => infer U} ? U : never
 
-export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, AppStateType, unknown, A>
+export type BaseThunkType<A extends Action, R = Promise<void>> = ThunkAction<R, RootState, unknown, A>
 
-export type AppStateType = ReturnType<typeof rootReducer>
+export type RootState = ReturnType<typeof store.getState>
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export type AppDispatch = ThunkDispatch<RootState, unknown, AnyAction>
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, AnyAction>
+
+
+
+
+
 
 
 
