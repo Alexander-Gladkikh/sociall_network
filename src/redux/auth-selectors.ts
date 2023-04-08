392ebd071e5b4077e17651b7a7d4c721dacd129b
Alexander <som17son25@gmail.com>
2023-04-08 22:08:19 +0300

    feat: create auth-selectors

---
+++ src/redux/auth-selectors.ts
@@ -0,0 +1,10 @@
+import {createSelector} from "reselect";
+import {RootState} from "./redux-store";
+
+export const selectIsAuth = (state: RootState) => {
+    return state.auth.isAuth
+}
+export const selectCurrentUserLogin = (state: RootState) => {
+    return state.auth.login
+}
+
