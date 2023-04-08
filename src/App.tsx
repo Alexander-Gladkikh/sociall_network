d17a51cb852f423c9f308f71c3647adc5cdaff8f
Alexander <som17son25@gmail.com>
2023-04-08 22:02:57 +0300

    feat: App create layout AntDesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import React, {lazy, Suspense} from 'react';
 import './App.css';
-import {Route, Routes} from "react-router-dom";
-import {Music} from "./componets/Music/Music";
-import {Settings} from "./componets/Settings/Settings";
-import NavbarContainer from "./componets/Navbar/NavbarContainer";
-import {UsersPage} from "./componets/Users/UsersContainer";
 import {withRouter} from "./componets/Profile/ProfileContainer";
-import HeaderContainer from "./componets/Header/HeaderContainer";
-import LoginPage from "./componets/login/Login";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import {Preloader} from "./componets/common/preloader/Preloader";
 import {RootState} from "./redux/redux-store";
+import {Avatar, Breadcrumb, Col, Layout, Menu, Row} from "antd";
+import {Link, NavLink, Route, Routes} from "react-router-dom";
+import {UsersPage} from "./componets/Users/UsersContainer";
+import {Settings} from "./componets/Settings/Settings";
+import Login from "./componets/login/Login";
+import {Music} from "./componets/Music/Music";
+import {UserOutlined} from '@ant-design/icons';
+import {Header} from "./componets/Header/Header";
 
+const {SubMenu} = Menu
+const {Content, Sider} = Layout;
 
 const DialogsContainer = lazy(() => import('./componets/Dialogs/DialogsContainer'));
 const ProfileContainer = lazy(() => import('./componets/Profile/ProfileContainer'));
@@ -21,57 +24,109 @@ const ProfileContainer = lazy(() => import('./componets/Profile/ProfileContainer
 
 type MapPropsType = ReturnType<typeof mapStateToProps>
 type DispatchPropsType = {
-    initializeApp: () => void
+  initializeApp: () => void
 }
+
 class App extends React.Component<MapPropsType & DispatchPropsType> {
 
-    catchAllUnhandledErrors = (e: Event) => {
-        alert('Some error occured')
+
+  catchAllUnhandledErrors = (e: Event) => {
+    alert('Some error occured')
+  }
+
+  componentDidMount() {
+    this.props.initializeApp()
+    window.addEventListener('unhandlerejection', this.catchAllUnhandledErrors)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('unhandlerejection', this.catchAllUnhandledErrors)
+  }
+
+  render() {
+    //ts-ignore
+    if (!this.props.initialized) {
+      return <Preloader/>
     }
 
-    componentDidMount() {
-        this.props.initializeApp()
-        window.addEventListener('unhandlerejection', this.catchAllUnhandledErrors)
-    }
+    return (
+      <Layout>
+        <Header/>
+        <Layout>
+          <Sider width={200} style={{background: "cyan"}}>
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={['1']}
+              // defaultOpenKeys={['sub1']}
+              style={{height: '100%', borderRight: 0}}
+            >
+              <SubMenu key='sub1' icon={<UserOutlined/>} title='My Profile'>
+                <Menu.Item key='1'><Link to='/profile'>Profile</Link></Menu.Item>
+                <Menu.Item key='2'><Link to='/dialogs'>Messages</Link></Menu.Item>
+              </SubMenu>
+              <SubMenu key='sub2' icon={<UserOutlined/>} title='Developers'>
+                <Menu.Item key='3'><Link to='/users'>Developers</Link></Menu.Item>
 
-    componentWillUnmount() {
-        window.removeEventListener('unhandlerejection', this.catchAllUnhandledErrors)
-    }
-
-    render() {
-        // @ts-ignore
-        if (!this.props.initialized) {
-            return <Preloader/>
-        }
-        return (
-                <div className={'app-wrapper'}>
-                    <HeaderContainer/>
-                    <NavbarContainer/>
-                    <div className={'app-wrapper-content'}>
-                        <Suspense fallback={<div>Loading...</div>}>
-                            <Routes>
-                                <Route path="/profile/:profileId" element={<ProfileContainer/>}/>
-                                <Route path="/dialogs/*" element={<DialogsContainer/>}/>
-                                <Route path="/users/*" element={<UsersPage/>}/>
-                                <Route path='/music/*' element={<Music/>}/>
-                                <Route path='/settings/*' element={<Settings/>}/>
-                                <Route path='/profile' element={<ProfileContainer/>}/>
-                                <Route path='/login' element={<LoginPage/>}/>
-                            </Routes>
-                        </Suspense>
-                    </div>
-                </div>
-        );
-    }
+              </SubMenu>
+            </Menu>
+          </Sider>
+          <Layout style={{padding: '0 24px 24px'}}>
+            <Breadcrumb style={{margin: '16px 0'}}>
+              <Breadcrumb.Item>Home</Breadcrumb.Item>
+              <Breadcrumb.Item>List</Breadcrumb.Item>
+              <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <Content
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 280,
+                background: "cyan",
+              }}
+            >
+              <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                  <Route path="/profile/:profileId" element={<ProfileContainer/>}/>
+                  <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+                  <Route path="/users/*" element={<UsersPage/>}/>
+                  <Route path='/music/*' element={<Music/>}/>
+                  <Route path='/settings/*' element={<Settings/>}/>
+                  <Route path='/profile' element={<ProfileContainer/>}/>
+                  <Route path='/login' element={<Login/>}/>
+                </Routes>
+              </Suspense>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+      // <div className={'app-wrapper'}>
+      //     <HeaderContainer/>
+      //     <NavbarContainer/>
+      //     <div className={'app-wrapper-content'}>
+      //         <Suspense fallback={<div>Loading...</div>}>
+      //             <Routes>
+      //                 <Route path="/profile/:profileId" element={<ProfileContainer/>}/>
+      //                 <Route path="/dialogs/*" element={<DialogsContainer/>}/>
+      //                 <Route path="/users/*" element={<UsersPage/>}/>
+      //                 <Route path='/music/*' element={<Music/>}/>
+      //                 <Route path='/settings/*' element={<Settings/>}/>
+      //                 <Route path='/profile' element={<ProfileContainer/>}/>
+      //                 <Route path='/login' element={<Login/>}/>
+      //             </Routes>
+      //         </Suspense>
+      //     </div>
+      // </div>
+    );
+  }
 }
 
 const mapStateToProps = (state: RootState) => ({
-    initialized: state.app.initialized
+  initialized: state.app.initialized
 })
 
 export default compose<React.ComponentType>(
-    withRouter,
-    connect(mapStateToProps, {initializeApp}))(App);
+  withRouter,
+  connect(mapStateToProps, {initializeApp}))(App);
 
 
 
