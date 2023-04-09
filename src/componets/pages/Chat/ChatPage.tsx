f24cee8e964319e8d6ecd14e32722d81e6471dc9
Alexander <som17son25@gmail.com>
2023-04-09 10:49:39 +0300

    ChatPage: create WebSocket channel

--- src/componets/pages/Chat/ChatPage.tsx
+++ src/componets/pages/Chat/ChatPage.tsx
@@ -1,4 +1,14 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from "react-redux";
+
+const wsChannel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+
+type ChatMessageType = {
+  message: string
+  photo: string
+  userId: number
+  userName: string
+}
 
 const ChatPage: React.FC = () => {
   return (
@@ -10,6 +20,7 @@ const ChatPage: React.FC = () => {
 export default ChatPage
 
 const Chat: React.FC = () => {
+
   return (
     <div>
       <Messages/>
@@ -19,37 +30,51 @@ const Chat: React.FC = () => {
 };
 
 const Messages: React.FC = () => {
-  const messages = [1,2,3,4]
+
+  const [messages, setMessages] = useState<ChatMessageType[]>([])
+
+  useEffect(() => {
+    wsChannel.addEventListener('message', (e) => {
+      let newMessages = JSON.parse((e.data));
+      setMessages((prevMessages) => [...prevMessages, ...newMessages])
+    })
+  }, [])
+
   return (
     <div style={{height: '500px', overflow: 'auto'}}>
-      {messages.map((m: any) => <Message />)}
-      {messages.map((m: any) => <Message />)}
-      {messages.map((m: any) => <Message />)}
+      {messages.map((m, index) => <Message key={index} message={m}/>)}
     </div>
   );
 };
 
-const Message: React.FC = () => {
-  const message = {
-    url: 'https://via.placeholder.com/30',
-    author: 'Aaaaa',
-    text: 'Hello World'
-  }
+const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
+
   return (
     <div>
-      <img src={message.url} alt=""/> <b>{message.author}</b>
+      <img src={message.photo} alt=""/> <b>{message.userName}</b>
       <br/>
-      {message.text}
+      {message.message}
       <hr/>
     </div>
   );
 };
 
 const AddMessageForm: React.FC = () => {
+
+  const [message, setMessage] = useState('')
+  const sendMessage = () => {
+    if (!message) {
+      return
+    }
+    wsChannel.send(message)
+    setMessage('')
+  }
+
   return (
-    <div>
-      <textarea></textarea>
-      <button>Send</button>
-    </div>
-  );
+  <div>
+    <textarea onChange={(e) => setMessage(e.currentTarget.value)} value={message}></textarea>
+    <button onClick={sendMessage}>Send</button>
+  </div>
+)
+  ;
 };
